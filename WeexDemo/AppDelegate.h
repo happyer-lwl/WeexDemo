7633b73a8c7b2669ff921a364a56242d232103b2
happyer-lwl <runlwl@163.com>
2016-12-08 09:58:48 +0800

    Initial Commit

---
+++ WeexDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeexDemo
+//
+//  Created by WyzcWin on 16/12/8.
+//  Copyright © 2016年 lwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeexDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeexDemo
+//
+//  Created by WyzcWin on 16/12/8.
+//  Copyright © 2016年 lwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
